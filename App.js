292b0b6e48859bd098baaca02e4a2d2b3d37aaa3
Nathan Rocha Gomes <nathan.326423@alunosatc.edu.br>
2026-03-18 19:28:49 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>ROJÃO</Text>
+      <Button title='Aperte o botão' onPress={() => alert('CABUM!')} />
+      <Image
+        source={require('./assets/Axalalala.jpg')}
+        style={{ width: 200, height: 200 }}
+      />
+     <StatusBar style="auto" />    
     </View>
   );
 }
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1572c9ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ README.md
@@ -0,0 +1,13 @@
+# cabum-mobile
+
+Projeto mobile (Expo / React Native). Conteúdo inicial enviado.
+
+Remote repository: https://github.com/NathanRochaGomes/Mobile_App.git
+
+Como rodar (exemplo):
+
+```powershell
+cd c:\Users\nathan.326423\cabum-mobile
+npm install
+npx expo start
+```
